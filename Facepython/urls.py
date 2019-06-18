07ca2c27a6b27eaf805e8d01ac6b284bb393c7a7
freedomDR <zjrforeverdr@gmail.com>
2019-06-18 14:49:27 +0800

    移除验证码

--- Facepython/urls.py
+++ Facepython/urls.py
@@ -21,14 +21,11 @@ from users.api import face
 from users.api import stream
 from django.conf.urls.static import static
 from django.conf import settings
-from users.api import captcha as cap
 from users.api import check
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r"^captcha/", include('captcha.urls')),
-    url(r"^captcha", cap.captcha_view),
-    url(r"^refresh_captcha", cap.captcha_view),
     url(r'^api-token-auth', views.obtain_auth_token),
     url(r'^api/user/login',user.obtain_auth_token),
     url(r'^api/sys/user/info',user.user_profile),
